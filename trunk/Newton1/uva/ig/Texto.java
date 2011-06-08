8c6bd1ca36e1be936aa88ad62f2fea52f4e8ff63
mancotr@gmail.com <mancotr@gmail.com@b4ce7654-4f56-18c2-2309-936b518719a6>
2011-06-08 18:50:11 +0000

    clase para imprimir textos en el proyecto.

---
+++ trunk/Newton1/uva/ig/Texto.java
@@ -0,0 +1,72 @@
+package uva.ig;
+
+import javax.media.opengl.GL;
+import com.sun.opengl.util.GLUT;
+import java.awt.*;
+
+/**
+ *
+ * @author Cotri
+ */
+
+/**
+ * Clase que imprime texto en OpenGL.
+ *
+ */
+public class Texto implements IObjetoGL{
+
+    GLUT glut=new GLUT();
+
+    public Texto(){
+
+    }
+
+    public void dibujar(GL gl){
+    }
+
+    
+    /**
+     *
+     * @param gl el gl que usamos en el display
+     * @param x posicion x donde queremos que empiece a pintarse
+     * @param y posicion y donde queremos que empiece a pintarse
+     * @param text texto a imprimir
+     * @param color color del texto
+     */
+    public void dibujar(GL gl, float x, float y, String text, Color color){
+        char c;
+        boolean lightingState;
+        gl.glPushMatrix();
+        gl.glTranslatef(x, y, 0);
+        float height = glut.glutStrokeLengthf(GLUT.STROKE_ROMAN, "X");
+        float factor = 0.5f / height;
+        gl.glScaled(factor, factor, 0);
+        gl.glTranslatef(x, y, 0);
+        lightingState = gl.glIsEnabled(GL.GL_LIGHTING);
+        gl.glDisable(GL.GL_LIGHTING);
+        gl.glColor3d(color.getRed() / 255.0, color.getGreen() / 255.0, color.getBlue() / 255.0);
+
+        //nos da el grosor
+        gl.glLineWidth(2.0f);
+        for (int offset = 0; offset < text.length(); offset++) {
+            c = text.charAt(offset);
+            glut.glutStrokeCharacter(GLUT.STROKE_ROMAN, c);
+        }
+        if (lightingState) {
+            gl.glEnable(GL.GL_LIGHTING);
+        }
+        gl.glPopMatrix();
+    }
+
+    public void cambiarPosicion (float x, float y, float z){
+
+    }
+
+    public void cambiarGiro (float gx, float gy, float gz){
+
+    }
+
+    public void cambiarEscalado (float x, float y, float z){
+
+    }
+}
